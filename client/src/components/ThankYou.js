92318c288634aeb7b7da9ab82e09c1046d051bf8
Hasib Bin Rafique <hasib25@gmail.com>
2020-11-11 14:31:21 +0600

    Thank you page updated

--- client/src/components/ThankYou.js
+++ client/src/components/ThankYou.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 class ThankYou extends Component {
   render() {
     return (
-    <div class="jumbotron">
-        <div class="container mt-5">
-            <h2>Thanks For Registration</h2>
-            <h3>You can now go to your desired destination.</h3>
-            <h4>Have a nice day at BHTPA.</h4>
-            
-            <p><a class="btn btn-lg btn-success" href="/new_visitor" role="button">Register As a Guest</a></p>
+      <div className="container">
+      <div className="jumbotron mt-5">
+        <div className="col-md-6 col-sx-6 mx-auto">
+          <h1 className="text-center">Thanks For Registration</h1>
+          <h3 className="text-center mb-5">
+              Now You Can Go To Your Desired Destination.
+          </h3>
+          <p className="text-center"><a className="btn btn-block btn-lg btn-success" style={{fontSize:"xx-large"}} href="/new_visitor" role="button">Register As a Guest</a></p>
         </div>
+      </div>
     </div>
     )
   }
